b0dc73a423677a813261ec66749e5e9c591c2f68
ryanrf1986 <ryanfleming@ymail.com>
2022-05-12 14:45:23 -0700

    commit

--- fakerAPI/Companies.js
+++ fakerAPI/Companies.js
@@ -7,6 +7,7 @@ class Companies {
         this.name = faker.company.companyName()
         this.address = {
             street : faker.address.streetAddress(),
+            city : faker.address.city(),
             state : faker.address.state(),
             zipCode : faker.address.zipCode(),
             country : faker.address.country()
