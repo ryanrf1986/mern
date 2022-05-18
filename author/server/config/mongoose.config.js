551f51a3ccb230020b73bd5d8337a6a3ceb00301
ryanrf1986 <ryanfleming@ymail.com>
2022-05-18 11:21:56 -0700

    changed product name

--- author/server/config/mongoose.config.js
+++ author/server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1/authors", {
+mongoose.connect("mongodb://127.0.0.1/author", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
